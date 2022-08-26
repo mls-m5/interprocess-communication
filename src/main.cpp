8584dda97c94fe1dff20af779ffa7b7f01a73eb9
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-08-26 12:53:03 +0200

    wrap host code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 
 // This is what will actually be called
 int testFunction(float x, std::string str) {
-    std::cout << str;
+    std::cout << "in testFunction:"
+              << "\n  str = ";
+    std::cout << str << std::endl;
     return static_cast<int>(x);
 }
 
@@ -24,6 +26,7 @@ std::string typeDescription<std::string>() {
 class InArchive {
     std::stringstream ss;
 
+    // This could be implemented differently. raw TCP would differ from url etc
     template <typename T>
     void unpackSingleArg(T &value) {
         ss >> value;
@@ -42,44 +45,68 @@ public:
 
         return ret;
     }
+
+    template <typename T>
+    T read() {
+        auto ret = T{};
+        unpackSingleArg(ret);
+        return ret;
+    }
 };
 
-// Should probably not be a global variable in the future
-auto funcMap =
-    std::unordered_map<std::string, std::function<void(InArchive &)>>{};
+struct FunctionHost {
+    // Should probably not be a global variable in the future
+    std::unordered_map<std::string, std::function<void(InArchive &)>> funcMap;
 
-// Register host std::function
-template <typename R, typename... Args>
-void registerFunction(std::string name, std::function<R(Args...)> f) {
-    std::cout << "register function";
-    ((std::cout << ',' << typeDescription<Args>()), ...) << std::endl;
+    /// Register host std::function
+    template <typename R, typename... Args>
+    void registerFunction(std::string name, std::function<R(Args...)> f) {
+        std::cout << "register function";
+        ((std::cout << ',' << typeDescription<Args>()), ...) << std::endl;
 
-    auto callback = [f](InArchive &archive) {
-        auto argsTuple = archive.unpack<Args...>();
-        std::apply(f, argsTuple);
-    };
+        auto callback = [f](InArchive &archive) {
+            auto argsTuple = archive.unpack<Args...>();
+            std::apply(f, argsTuple);
+        };
 
-    funcMap.insert({name, callback});
-}
+        this->funcMap.insert({name, callback});
+    }
 
-// Register any type that can be converted to std::function
-template <typename F>
-void registerFunction(std::string name, F f) {
-    registerFunction(std::move(name), std::function{f});
-}
+    /// Register any type that can be converted to std::function
+    template <typename F>
+    void registerFunction(std::string name, F f) {
+        registerFunction(std::move(name), std::function{f});
+    }
+
+    void handleArchive(InArchive &archive) {
+        auto name = archive.read<std::string>();
+        if (auto f = funcMap.find(name); f != funcMap.end()) {
+            f->second(archive);
+        }
+        else {
+            throw std::runtime_error{"function " + name +
+                                     " from archive was not found"};
+        }
+    }
+};
 
 int main(int argc, char *argv[]) {
-    registerFunction("testFunction", testFunction);
+    auto host = FunctionHost{};
+
+    host.registerFunction("testFunction", testFunction);
 
     // Test
-    for (auto &it : funcMap) {
+    for (auto &it : host.funcMap) {
         std::cout << it.first << std::endl;
     }
 
     // This should be replaced with automatic handling
-    auto archive = InArchive{"2.\nhello"};
+    auto archive = InArchive{R"(
+testFunction
+2.
+hello)"};
 
-    funcMap["testFunction"](archive);
+    host.handleArchive(archive);
 
     return 0;
 }
